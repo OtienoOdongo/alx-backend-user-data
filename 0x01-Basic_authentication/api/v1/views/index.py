67dfcda7ee13ac5bb11af08194d2e026d412a07c
OtienoOdongo <otabiznake@gmail.com>
2023-11-07 09:18:31 +0000

    error handler

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -34,3 +34,13 @@ def unauthorized() -> str:
     - unauthorized error message
     """
     abort(401)
+
+
+@app_views.route('/forbidden/', strict_slashes=False)
+def forbidden() -> str:
+    """
+    GET /api/v1/forbidden
+    Return:
+    - user forbidden from accessing a resource
+    """
+    abort(403)
