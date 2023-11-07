ae526a0e16e1927c87ddc31d22688664183e033e
OtienoOdongo <otabiznake@gmail.com>
2023-11-07 13:14:23 +0000

    base64 extrcation

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -11,4 +11,31 @@ class BasicAuth(Auth):
     """
     a BasicAuth class that inherits from Auth
     """
-    pass
+    def extract_base64_authorization_header(
+        self,
+        authorization_header: str
+    ) -> str:
+        """
+        Extract the Base64-encoded credentials
+        from a Basic Authentication header.
+
+        Args:
+            authorization_header (str):
+                Authorization header string containing
+                Basic Authentication credentials.
+
+        Returns:
+            str:
+                The Base64 part of the Authorization
+                header for Basic Authentication.
+        """
+        if (
+            authorization_header is None
+            or not isinstance(authorization_header, str)
+            or not authorization_header.startswith('Basic ')
+        ):
+            return None
+
+        # Extracting the Base64 part after 'Basic '
+        base64_encoded = authorization_header[len('Basic '):]
+        return base64_encoded
