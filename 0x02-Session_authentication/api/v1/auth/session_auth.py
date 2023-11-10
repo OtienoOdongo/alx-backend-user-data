c38b030060a501ad0d6e6c2e6a518b8cc4aaba90
OtienoOdongo <otabiznake@gmail.com>
2023-11-10 12:30:03 +0000

    session authentication

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -55,7 +55,7 @@ class SessionAuth(Auth):
 
         return self.user_id_by_session_id.get(session_id)
 
-    def current_user(self, request=None) -> TypeVar('User'):
+    def current_user(self, request=None):
         """
         Returning a User instance based on a cookie value.
 
