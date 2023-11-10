7e262ffa4b48b5574ab629a111b252e61667a629
OtienoOdongo <otabiznake@gmail.com>
2023-11-10 11:46:28 +0000

    session authentication

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -4,10 +4,35 @@
 
 from api.v1.auth.auth import Auth
 from models.user import User
+import uuid
 
 
 class SessionAuth(Auth):
     """
     a class that uses session to authenticate users
     """
-    pass
+    # Class attribute to store user_id by session_id
+    user_id_by_session_id = {}
+
+    def create_session(self, user_id: str = None) -> str:
+        """
+        Creating a Session ID for a user_id.
+
+        Args:
+            user_id (str):
+            The user's ID to associate with the session.
+
+        Returns:
+            str:
+            A generated Session ID or None.
+        """
+        if user_id is None or type(user_id) is not str:
+            return None
+
+        # Generating a Session ID using uuid4
+        session_id = str(uuid.uuid4())
+
+        # Storing the user_id in the dictionary using session_id as the key
+        self.user_id_by_session_id[session_id] = user_id
+
+        return session_id
